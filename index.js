01e357dee2ecf78ee86e9fb2a1706c8fb1d1aed2
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2020-12-01 14:00:55 -0800

    made the intial completion suggester work, by indexing, mapping and suggesting the searched data with the help of client APIs

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ async function run () {
     index: 'game-of-thrones',
     body: {
       character: 'Ned Stark',
-      quote: 'Winter is coming.'
+      quote: 'Winter is coming.',
+      suggest: {
+        input: 'Ned Stark'.split(' '),
+        output: this.character
+      }
     }
   })
 
@@ -23,38 +27,84 @@ async function run () {
     index: 'game-of-thrones',
     body: {
       character: 'Daenerys Targaryen',
-      quote: 'I am the blood of the dragon.'
+      quote: 'I am the blood of the dragon.',
+      suggest: {
+        input: 'Daenerys Targaryen'.split(' '),
+        output: this.character
+      }
     }
   })
 
   await client.index({
     index: 'game-of-thrones',
-    // here we are forcing an index refresh,
-    // otherwise we will not get any result
-    // in the consequent search
     refresh: true,
     body: {
       character: 'Tyrion Lannister',
-      quote: 'A mind needs books like a sword needs a whetstone.'
-    }
-  })
-
-  // Let's search!
-  const { body } = await client.search({
-    index: 'game-of-thrones',
-    body: {
-      query: {
-        match: {
-          quote: 'winter'
-        }
+      quote: 'A mind needs books like a sword needs a whetstone.',
+      suggest: {
+        input: 'Tyrion Lannister'.split(' '),
+        output: this.character
       }
     }
   })
-
-  console.log(body.hits.hits)
 }
 
-run().catch(console.log)
+const checkIndices = async () => {
+  client.indices.exists({index: 'game-of-thrones'})
+  .then(response => {
+    if(!response.body) {
+      client.indices.create( { index: 'game-of-thrones'})
+      console.log('Succcessfully created new index')
+    }
+    console.log('Index has already been created')
+  })
+  .catch(error => console.log('Error is', error))
+}
+
+const putMap = async () => {
+
+  client.indices.putMapping({
+   index: 'game-of-thrones',
+   body: {
+     properties: {
+       character: { type: "text" },
+       quote: { type: "text" },
+       suggest: {
+           type: "completion",
+           analyzer: "simple"
+       }
+     }
+   }
+ }, (err, res, status) => {
+   if (err) {
+     console.error('Error is' , err, status)
+   }
+   else {
+     console.log('Successfully created mapping', status, res)
+   }
+ });
+}
+
+const getSuggestions = async() => {
+   client.search({
+    index: 'game-of-thrones',
+    body: {
+          _source: "suggest",
+          suggest: {
+            gotsuggest : {
+              text: 'Ty', 
+              completion : {field : 'suggest'}
+            }
+          }
+        }
+  }).then(res => console.log('Response is', res))
+  .catch(err => console.log('Error is', err))
+}
+
+//run().catch(console.log)
+checkIndices()
+// putMap()
+// getSuggestions()
 
 const PORT = process.env.PORT || 5000
 
